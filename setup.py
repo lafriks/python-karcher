22e21abcb244ce68adb85db16c54c4ded61953da
Lauris BH <lauris@nix.lv>
2023-04-26 15:48:47 +0300

    Packaging fixes

--- setup.py
+++ setup.py
@@ -4,16 +4,19 @@ except ImportError:
     from distutils.core import setup
 
 setup(
-    name = 'karcher',
-    packages = ['karcher'],
+    name='karcher',
+    packages=['karcher'],
     include_package_data=True,
-    version = '0.1',
+    version='0.1',
     license='MIT',
-    description = 'Kärcher Home Robots client',
-    author = 'Lauris BH',
-    author_email = 'lauris@nix.lv',
-    url = 'https://github.com/lafriks/karcher',
-    download_url = 'https://github.com/lafriks/karcher/archive/v_0.1.tar.gz',
+    description='Kärcher Home Robots client',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Lauris BH',
+    author_email='lauris@nix.lv',
+    url='https://github.com/lafriks/karcher',
+    download_url='https://github.com/lafriks/karcher/archive/v0.1.tar.gz',
+    platforms='any',
     install_requires=[
         'requests',
         'tzlocal',
