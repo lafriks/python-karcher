67362f184f0ebd0ac0477699fd9f1df08a78d95a
Lauris BH <lauris@nix.lv>
2023-04-27 18:05:07 +0300

    Refactor session handling and option to subscribe to device property updates

--- karcher/cli.py
+++ karcher/cli.py
@@ -18,18 +18,20 @@ try:
 except ImportError:
     echo = click.echo
 
+
 class EnhancedJSONEncoder(json.JSONEncoder):
     def default(self, o):
         if dataclasses.is_dataclass(o):
             return dataclasses.asdict(o)
         return super().default(o)
 
+
 class GlobalContextObject:
     def __init__(self,
-        debug: int = 0,
-        output: str = "json",
-        region: Region = Region.EU
-    ):
+                 debug: int = 0,
+                 output: str = "json",
+                 region: Region = Region.EU
+                 ):
         self.debug = debug
         self.output = output
         self.region = region
@@ -43,6 +45,7 @@ class GlobalContextObject:
         else:
             echo(json.dumps(result, cls=EnhancedJSONEncoder))
 
+
 @click.group()
 @click.option("-d", "--debug", is_flag=True)
 @click.option(
@@ -53,7 +56,7 @@ class GlobalContextObject:
     help="Output format. Default: 'json'")
 @click.option(
     "-r",
-    "--region", 
+    "--region",
     type=click.Choice([Region.EU, Region.US, Region.CN]),
     default=Region.EU,
     help="Region of the server to query. Default: 'eu'")
@@ -68,6 +71,7 @@ def cli(ctx: click.Context, debug: int, output: str, region: Region):
 
     ctx.obj = GlobalContextObject(debug=debug, output=output, region=region)
 
+
 def safe_cli():
     try:
         cli()
@@ -77,6 +81,7 @@ def safe_cli():
             'message': ex.message
         }))
         return
+
 
 @cli.command()
 @click.pass_context
@@ -88,6 +93,7 @@ def get_urls(ctx: click.Context):
 
     ctx.obj.print(d)
 
+
 @cli.command()
 @click.option('--username', '-u', help='Username to login with.')
 @click.option('--password', '-p', help='Password to login with.')
@@ -98,6 +104,7 @@ def login(ctx: click.Context, username: str, password: str):
     kh = KarcherHome(region=ctx.obj.region)
     ctx.obj.print(kh.login(username, password))
 
+
 @cli.command()
 @click.option('--username', '-u', default=None, help='Username to login with.')
 @click.option('--password', '-p', default=None, help='Password to login with.')
@@ -107,18 +114,18 @@ def devices(ctx: click.Context, username: str, password: str, token: str):
     """List all devices."""
 
     kh = KarcherHome(region=ctx.obj.region)
-    sess = None
     if token is not None:
-        sess = Session.from_token(token, '')
+        kh.login_token(token, '')
     elif username is not None and password is not None:
-        sess = kh.login(username, password)
+        kh.login(username, password)
     else:
-        raise click.BadParameter('Must provide either token or username and password.')
+        raise click.BadParameter(
+            'Must provide either token or username and password.')
 
-    devices = kh.get_devices(sess)
+    devices = kh.get_devices()
 
     # Logout if we used a username and password
     if token is None:
-        kh.logout(sess)
+        kh.logout()
 
     ctx.obj.print(devices)
---
+++ karcher/mqtt.py
@@ -0,0 +1,88 @@
+import ssl
+from paho.mqtt.client import Client, MQTTv311
+
+from .utils import get_random_device_id
+
+
+class MqttClient:
+    def __init__(self, host, port, username, password):
+        self._host = host
+        self._port = port
+        self._username = username
+        self._password = password
+        self._topics = []
+        self._client = Client(
+            client_id=self._username + '_' + get_random_device_id(),
+            clean_session=True,
+            protocol=MQTTv311)
+        self.on_message = None
+
+    def connect(self):
+        # TODO validate certificate
+        self._client.tls_set(cert_reqs=ssl.CERT_NONE)
+        self._client.tls_insecure_set(True)
+        self._client.username_pw_set(self._username, self._password)
+        self._client.on_connect = self._on_connect
+        self._client.on_message = self._on_message
+        # TODO: add option to enable logging
+        # self._client.on_log = lambda client, userdata, level, buf: print(buf)
+        self._client.connect(self._host, self._port, 60)
+        self._client.loop_start()
+
+    def disconnect(self):
+        self._client.loop_stop()
+        self._client.disconnect()
+
+    def _subscribe(self, topics):
+        if len(topics) == 0:
+            return
+        t = []
+        for topic in topics:
+            t.append((topic, 0))
+        self._client.subscribe(t)
+
+    def subscribe(self, topics):
+        t = []
+        for topic in topics:
+            if topic not in self._topics:
+                self._topics.append(topic)
+                t.append(topic)
+        if self._client.is_connected():
+            self._subscribe(t)
+
+    def unsubscribe(self, topics):
+        t = []
+        for topic in topics:
+            if topic in self._topics:
+                self._topics.remove(topic)
+                t.append(topic)
+        if self._client.is_connected():
+            self._client.unsubscribe(t)
+
+    def publish(self, topic, payload):
+        self._client.publish(topic, payload)
+
+    def _on_connect(self, client, userdata, flags, rc):
+        self._subscribe(self._topics)
+
+    def _on_message(self, client, userdata, msg):
+        if self.on_message is not None:
+            self.on_message(msg.topic, msg.payload)
+
+    def __del__(self):
+        self.disconnect()
+
+
+def get_device_topics(product_id: str, sn: str):
+    return [
+        '/mqtt/' + product_id + '/' + sn + '/thing/event/property/post',
+        '/mqtt/' + product_id + '/' + sn + '/thing/service/property/set_reply',
+        '/mqtt/' + product_id + '/' + sn + '/thing/service/property/get_reply',
+        '/mqtt/' + product_id + '/' + sn + '/thing/service_invoke',
+        '/mqtt/' + product_id + '/' + sn + '/thing/service_invoke_reply/#',
+        '/mqtt/' + product_id + '/' + sn + '/thing/event/cur_path/post',
+        '/mqtt/' + product_id + '/' + sn + '/ota/service/upgrade/set_reply',
+        '/mqtt/' + product_id + '/' + sn + '/ota/service/upgrade/post',
+        '/mqtt/' + product_id + '/' + sn + '/ota/service/upgrade/get_reply',
+        '/mqtt/' + product_id + '/' + sn + '/ota/service/version/post',
+    ]
