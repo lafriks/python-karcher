580b6d808742ecff6ea1c0f465bb37ad05764c7b
Lauris BH <lauris@nix.lv>
2023-10-03 15:31:31 +0300

    Release v0.5.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name='karcher-home',
     packages=['karcher'],
     include_package_data=True,
-    version='0.5',
+    version='0.5.1',
     license='MIT',
     description='Kärcher Home Robots client',
     long_description=open('README.md').read(),
@@ -15,7 +15,7 @@ setup(
     author='Lauris BH',
     author_email='lauris@nix.lv',
     url='https://github.com/lafriks/python-karcher',
-    download_url='https://github.com/lafriks/python-karcher/releases/download/v0.5/karcher-home-0.5.tar.gz',
+    download_url='https://github.com/lafriks/python-karcher/releases/download/v0.5.1/karcher-home-0.5.1.tar.gz',
     platforms='any',
     install_requires=[
         'click',
