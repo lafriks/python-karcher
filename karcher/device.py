lafriks/python-karcher

commit e0065b6d69d05b9e19524a8235874cc6378f9bc1

Fix mutable default values in dataclass
Fixes #3

karcher/device.py
@@ -101,7 +101,7 @@ class DeviceProperties:
     main_brush: int = 0
     side_brush: int = 0
     mop_life: int = 0
-    net_status: DevicePropertiesNetwork = DevicePropertiesNetwork()
+    net_status: DevicePropertiesNetwork
     repeat_state: int = 0
     tank_state: int = 0
     cloth_state: int = 0
@@ -121,7 +121,7 @@ class DeviceProperties:
     charge_state: int = 0
     back_to_wash: int = 0
     break_charging: int = 0
-    order_total: DevicePropertiesOrderTotal = DevicePropertiesOrderTotal()
+    order_total: DevicePropertiesOrderTotal
     memory_map: int = 0
     current_map_id: int = 0
     map_num: int = 0
@@ -131,15 +131,19 @@ class DeviceProperties:
     quiet_begin_time: int = 0
     quiet_end_time: int = 0
     broken_clean: int = 0
-    privacy: DevicePropertiesPrivacy = DevicePropertiesPrivacy()
+    privacy: DevicePropertiesPrivacy
     cur_path: List[float]
     dust_action: int = 0
     voice_type: int = 0
-    quiet_status: DevicePropertiesQuiet = DevicePropertiesQuiet()
+    quiet_status: DevicePropertiesQuiet
     last_update_time: int = 0
 
     def __init__(self, **kwargs):
         setattr(self, 'cur_path', [])
+        setattr(self, 'net_stauts', DevicePropertiesNetwork())
+        setattr(self, 'order_total', DevicePropertiesOrderTotal())
+        setattr(self, 'privacy', DevicePropertiesPrivacy())
+        setattr(self, 'quiet_status', DevicePropertiesQuiet())
         self.update(kwargs)
 
     def update(self, data: dict[str, Any]) -> bool:

commit 6c30e938122481a58d95e3dd99dfefae8c0c6e37

fix uninitialized property
When the robot is idle, parked at the base, and I call
`kh.get_device_properties(dev)`, I get back the following backtrace
(after removing some asyncio layers due to awaits):

   File "/nix/store/asiphbpiy2gmidfm3xbwcikayhs66289-python3-3.11.7/lib/python3.11/dataclasses.py", line 240, in wrapper
     result = user_function(self)
              ^^^^^^^^^^^^^^^^^^^
   File "<string>", line 3, in __repr__
 AttributeError: 'DeviceProperties' object has no attribute 'net_status'. Did you mean: 'net_stauts'?

What's particularly funny is that when run as `python -m asyncio`, to
get the `await` working in the REPL, the exception does not contain that
useful hint, and it looks like the following instead:

 >>> kh.get_device_properties(d[0])
 Traceback (most recent call last):
   File "/nix/store/asiphbpiy2gmidfm3xbwcikayhs66289-python3-3.11.7/lib/python3.11/concurrent/futures/_base.py", line 456, in result
     return self.__get_result()
            ^^^^^^^^^^^^^^^^^^^
   File "/nix/store/asiphbpiy2gmidfm3xbwcikayhs66289-python3-3.11.7/lib/python3.11/concurrent/futures/_base.py", line 401, in __get_result
     raise self._exception
   File "/nix/store/asiphbpiy2gmidfm3xbwcikayhs66289-python3-3.11.7/lib/python3.11/asyncio/__main__.py", line 34, in callback
     coro = func()
            ^^^^^^
   File "<console>", line 1, in <module>
   File "/nix/store/asiphbpiy2gmidfm3xbwcikayhs66289-python3-3.11.7/lib/python3.11/dataclasses.py", line 240, in wrapper
     result = user_function(self)
              ^^^^^^^^^^^^^^^^^^^
   File "<string>", line 3, in __repr__
 AttributeError: 'DeviceProperties' object has no attribute 'net_status'

Anyway, the root cause was just a typo in DeviceProperties.__init__.

Fixes: e0065b6 Fix mutable default values in dataclass

karcher/device.py
@@ -140,7 +140,7 @@ class DeviceProperties:
 
     def __init__(self, **kwargs):
         setattr(self, 'cur_path', [])
-        setattr(self, 'net_stauts', DevicePropertiesNetwork())
+        setattr(self, 'net_status', DevicePropertiesNetwork())
         setattr(self, 'order_total', DevicePropertiesOrderTotal())
         setattr(self, 'privacy', DevicePropertiesPrivacy())
         setattr(self, 'quiet_status', DevicePropertiesQuiet())